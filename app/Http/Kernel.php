5a27db6d8724d055d4518de00bfdaf1bdf56858b
jordimartinezjimenez <jordimj63@gmail.com>
2022-04-28 17:07:12 +0200

    API fixed

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,7 +41,8 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            'throttle:api',
+            // 'throttle:api',
+            // 'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
